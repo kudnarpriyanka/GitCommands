ed302881235ea7544e71a945d57f8d27fc83bdc8
priyanka <kudnarpriyanka14@gmail.com>
2024-05-29 20:34:53 +0530

    initial commit

---
+++ springbootbase/src/main/java/com/csi/SpringbootbaseApplication.java
@@ -0,0 +1,14 @@
+package com.csi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootbaseApplication.class, args);
+	
+	}
+
+}
---
+++ springbootbase/src/main/java/com/csi/controller/ApplicationController.java
@@ -0,0 +1,23 @@
+package com.csi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApplicationController {
+	
+	@GetMapping
+	public String sayHello() {
+		return "WELCOME TO FULL STACK Developer,pune";
+	}
+	
+	@GetMapping("/services")
+	public String sayServices() {
+		return "SOFTWARE SERVICES";
+	}
+	
+	
+
+}
